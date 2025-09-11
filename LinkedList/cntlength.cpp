86768b11d7bdcdfdc0e6a823329b3360ddbf3cb7
ketpals <bt19cse091@iiitn.ac.in>
2025-09-12 00:05:31 +0530

    Count Length of a LinkedList

---
+++ LinkedList/cntlength.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+    public:
+    Node(int data1, Node* next1){
+        data1 = data;
+        next1 = next;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+Node* arr2LL(vector<int>& v){
+    Node* head = new Node(v[0]);
+    Node* curr = head;
+    for(int i =1; i<v.size(); i++){
+        Node* temp = new Node(v[i]);
+        curr->next = temp;
+        curr = curr->next;
+    }
+    return head;
+}
+int len(Node* head){
+    Node* temp = head;
+    int cnt =0;
+    while(temp){
+    temp = temp->next;
+cnt++;
+    }
+    return cnt;
+}
+
+int main() {
+ vector<int> v = {88,2,3,4};
+ Node *y = arr2LL(v);
+ Node* temp = y;
+ cout<<len(temp);
+}
