9befd78521f628b55d3e8f7b94b4a57761d401ea
ketpals <bt19cse091@iiitn.ac.in>
2025-09-14 13:43:14 +0530

    Deletion of a node at kth position in LL

---
+++ LinkedList/Deletion/position.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* arr2LL(vector<int>& v){
+    Node* head = new Node(v[0]);
+    Node* curr = head;
+    for(int i=1; i<v.size(); i++){
+        Node* temp = new Node(v[i]);
+        curr->next = temp;
+        curr = curr->next;
+    }
+    return head;
+}
+int len(Node* head){
+    Node* temp = head;
+    int cnt =0;
+    while(temp){
+    temp = temp->next;
+cnt++;
+    }
+    return cnt;
+}
+Node* delpos(Node* head, int k) {
+    if (head == NULL || len(head) < k) {
+        return NULL;
+    }
+
+    // Case 1: Deleting the head node (k = 1)
+    if (k == 1) {
+        Node* temp = head;
+        head = head->next;
+        delete temp; // Use 'delete' for a single object
+        return head;
+    }
+
+    // Case 2: Deleting a node after the head
+    int cnt = 1;
+    Node* temp = head;
+    Node* prev = NULL;
+    while (temp != NULL) {
+        if (cnt == k) {
+            prev->next = temp->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+        cnt++;
+    }
+    return head;
+}
+int main(){
+    vector<int> v = {3,5,6,1,88};
+    int k = 5;
+    Node* head = arr2LL(v);
+    Node* temp = delpos(head,k);
+    while(temp){
+        cout<<temp->data<<" ";
+        temp= temp->next;
+    }
+
+}
+
+
